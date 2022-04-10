f194cf462d4119cacb8d29384255dbd5b159d13e
Dat Nguyen <tuj81499@temple.edu>
2022-04-10 00:17:14 -0400

    Create Client accept input

---
+++ Sudoku Smasher/src/SmasherServer/Client.java
@@ -0,0 +1,50 @@
+package SmasherServer;
+
+import java.io.*;
+import java.net.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    private Socket socket = null;
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
+
+    public boolean Client(int port){
+        try{
+            socket = New Socket("Host", port);
+            System.out.println("Client start!");
+            System.out.println("Sever-Client are connected!");
+
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+        }catch(IOException e)
+        {
+            System.out.println("Connection Error!");
+        }
+
+    }
+
+    public void sentInput(String lines){
+        // send input to server
+        try {
+            out.writeUTF(lines);
+        }catch(IOException e) {
+            System.out.println("Sending Error");
+        }
+
+    }
+
+    public String receiveSolver(){
+        String line = "";
+        try{
+            line = new String(in.readUTF());
+        }catch(IOEception e){
+            System.out.println("Client can not receive message!");
+            System.exit(0);
+        }
+    }
+}
